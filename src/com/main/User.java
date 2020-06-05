8419613864e94e6cb801092c5d61e1b06cb00d53
Lzh <420645952@qq.com>
2020-06-05 14:44:25 +0800

    add

---
+++ src/com/main/User.java
@@ -0,0 +1,44 @@
+package com.main;
+
+public class User {
+    private String name ;
+
+    private String phone;
+
+    private String addr;
+
+    private String nickeName;
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public String getNickeName() {
+        return nickeName;
+    }
+
+    public void setNickeName(String nickeName) {
+        this.nickeName = nickeName;
+    }
+}
