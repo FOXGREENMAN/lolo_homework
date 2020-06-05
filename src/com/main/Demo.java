10288416f45acd59fcdc1dffd2bd9b542b870461
Lzh <420645952@qq.com>
2020-06-05 16:46:59 +0800

    1

---
+++ src/com/main/Demo.java
@@ -0,0 +1,24 @@
+package com.main;
+
+public class Demo {
+    public static void main(String[] args) {
+        User two = new User();
+        String Page = "sdasdsad";
+        two.setName(Page);
+
+
+
+
+
+
+
+}
+
+}
+
+ class setNickeNameByName {
+    public static void main(String[] args) {
+
+
+    }
+}
