f4db0fc9e97d14a05deeafbd6af57c936ff16ce6
Lzh <420645952@qq.com>
2020-06-05 17:57:09 +0800

    1

--- src/com/main/june/Demo.java
+++ src/com/main/june/Demo.java
@@ -26,7 +26,7 @@ public class Demo {
     // 每个方法也要对齐, 同理是编码习惯~
     // 题目里, 返回值是无, 所以这里不需要返回值;
     // TODO 正确写法: public void setNickeNameByName(String nickName) // 标识符使用有意义的名字,空格每次使用一个
-    public  String setNickeNameByName(String one) {
+    public void  setNickeNameByName(String one) {
         User user = new User();
         String dateShortStr = DateUtils.getTodayShort();
         // 这里不对, setName是给user的name属性赋值, 所以你这里把日期字符串赋值给name了
@@ -38,12 +38,12 @@ public class Demo {
         // 所以这里接收的name已经不是原来的name了
         ioo = user.getName();
         // 这里创建了一个新的user对象, 为啥? 上面创建的那个user就没有使用了;
-        User iop = new User();
+
         // 这里给新建的user对象, 名字iop赋值了nickname属性, 但赋值的内容是name,所以是不对的;
         user.setNickeName(ioo);
 
         // 当你的返回值是void的时候, 就不用写return了
-        return null;
+
 
     }
 
