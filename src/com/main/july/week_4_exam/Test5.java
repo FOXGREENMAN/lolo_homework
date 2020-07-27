d46ce2553d2842986e9977fb9d4ab1bb833c8a6b
Lzh <420645952@qq.com>
2020-07-27 15:51:05 +0800

    1232

---
+++ src/com/main/july/week_4_exam/Test5.java
@@ -0,0 +1,16 @@
+package com.main.july.week_4_exam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test5 {
+    public static void main(String[] args) {
+        Map<String,Integer> map = new HashMap<String, Integer>();
+        map.put("appId",123213);
+
+    }
+    public static void sadsad( Map<String,Integer> map){
+                map.containsKey("sadasd");
+
+    }
+}
---
+++ src/com/main/july/week_4_exam/Test6.java
@@ -0,0 +1,20 @@
+package com.main.july.week_4_exam;
+
+public class Test6 {
+    public static void main(String[] args) {
+            int a = 1;
+            int c = 2;
+            int b = 3;
+
+            String s =c+"100";
+            s.substring(2);
+//        System.out.println(s);
+
+    }
+
+    public static int shuziwei(int a, int b, int c){
+
+
+            return a;
+    }
+}
