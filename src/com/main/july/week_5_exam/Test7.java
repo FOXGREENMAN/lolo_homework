0025b1246ca133872ed173e6f0ff2ef5053418db
Lzh <420645952@qq.com>
2020-07-28 20:00:33 +0800

    add test

--- src/com/main/july/week_5_exam/Test7.java
+++ src/com/main/july/week_5_exam/Test7.java
@@ -2,6 +2,7 @@ package com.main.july.week_5_exam;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Test7 {
     public static void main(String[] args) {
@@ -10,8 +11,12 @@ public class Test7 {
         sadsad(2,"d");
         sadsad(3,"q");
         sadsad(4,"v");*/
-       test('a');
-        System.out.println((int)'a');
+//       test('a');
+//        System.out.println((int)'a');
+
+        charMap();
+        test('c');
+
 
     }
     public static Map sadsad(int a, String b){
@@ -29,21 +34,34 @@ public class Test7 {
         map.put(Integer.valueOf((int)ch-96),String.valueOf(ch));
         return map;
     }
-
     public static Map charMap(){
-        Map<Integer,String> map = new HashMap<Integer, String>();
-        String alphStr = "ab2cedfghijklmnopqrsduvwxyz";
+        Map<String,Integer> map = new HashMap<String,Integer>();
 
+        String alphStr = "a,b,c,e,d,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,dasd";
         // 切割字符串 alphStr
-
         // 遍历切割后的数组
-
         // 循环调用alphe()方法建立map映射
+            String[] param = alphStr.split(",");
+            for (int i = 0; i < param.length; i++) {
+                map.put(param[i],i+1);
+//                System.out.println(map.get(i));
+            if (map.containsKey(param[i])){
+                System.out.println("成功");
+            }else {
+                System.out.println("失败");
+            }
+                System.out.println(map.containsKey(param[i]));
+         }
 
+
+
+        for (String key:map.keySet()) {
+//            System.out.println("map的key=" + key + ", map的value=" + map.get(key));
+        }
         return map;
     }
 
     public static void test(char ch){
-        System.out.println("字符:"+ch+"的位置:"+((int)ch-96));
+//       System.out.println("字符:"+ch+"的位置:"+((int)ch-96));
     }
 }
---
+++ src/com/main/july/week_5_exam/Test8.java
@@ -0,0 +1,7 @@
+package com.main.july.week_5_exam;
+
+public class Test8 {
+    public static void main(String[] args) {
+
+    }
+}
