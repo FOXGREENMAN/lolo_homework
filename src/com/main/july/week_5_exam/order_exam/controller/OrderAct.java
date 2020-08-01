d6321cb9bd419212176944640905739456307dbc
yeyuexin <yeyuexin@chainup.com>
2020-08-01 15:20:52 +0800

    add exam answer

--- src/com/main/july/week_5_exam/order_exam/controller/OrderAct.java
+++ src/com/main/july/week_5_exam/order_exam/controller/OrderAct.java
@@ -1,5 +1,9 @@
 package com.main.july.week_5_exam.order_exam.controller;
 
+import com.main.july.week_5_exam.order_exam.service.OrderService;
+
+import java.util.Map;
+
 /**
  * Controller层
  *
@@ -8,4 +12,31 @@ package com.main.july.week_5_exam.order_exam.controller;
  * Created by admin on 2020/7/30.
  */
 public class OrderAct {
+
+    /**
+     * 创建订单接口
+     * @param paraMap 创建订单数据
+     * @return
+     */
+    public boolean createOrder(Map<String,String> paraMap){
+
+        OrderService service = new OrderService();
+
+        boolean isSucc = service.createOrder(paraMap);
+
+        return isSucc;
+    }
+
+    /**
+     * 删除订单接口
+     * @param orderId 订单ID
+     * @return
+     */
+    public boolean delOrder(Long orderId){
+        OrderService service = new OrderService();
+
+        boolean isSucc = service.delOrder(orderId);
+
+        return isSucc;
+    }
 }
--- src/com/main/july/week_5_exam/order_exam/service/OrderService.java
+++ src/com/main/july/week_5_exam/order_exam/service/OrderService.java
@@ -33,5 +33,55 @@ public class OrderService {
         Store.delete(orderId);
     }
 
+    /**
+     * 创建订单对象
+     * @param paraMap
+     * @return
+     */
+    public boolean createOrder(Map<String,String> paraMap){
+        Order o = new Order();
+
+        Long orderId = Long.valueOf(System.currentTimeMillis());
+        o.setOrderId(orderId);
+        o.setSide(paraMap.get("side"));
+        o.setPrice(Double.valueOf(paraMap.get("price")));
+        o.setPrice(Double.valueOf(paraMap.get("volume")));
+
+        // 保存订单
+        return saveOrder(o);
+    }
+
+    /**
+     * 保存订单对象
+     * @param order
+     * @return
+     */
+    public boolean saveOrder(Order order){
+        Map<Long, Order> orderDb = DatabaseHungry.getInstance().dbMap;
+
+        // 订单已存在则不保存
+        if(orderDb.containsKey(order.getOrderId())){
+            System.out.println("订单对象已存在, 订单ID:"+order.getOrderId());
+            return false;
+        }
+        orderDb.put(order.getOrderId(),order);
+        return true;
+    }
+
+    /**
+     * 删除订单方法
+     * @param orderId
+     * @return
+     */
+    public boolean delOrder(Long orderId){
+        Map<Long, Order> orderDb = DatabaseHungry.getInstance().dbMap;
+
+        if(!orderDb.containsKey(orderId)){
+            System.out.println("订单对象不存在, 删除失败, 订单ID:" + orderId);
+            return false;
+        }
+        orderDb.remove(orderId);
+        return true;
+    }
 
 }
