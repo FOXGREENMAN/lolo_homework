133df3276758930bed3b1e5ac0d887eb9811b0fd
yeyuexin <yeyuexin@chainup.com>
2020-08-05 19:09:16 +0800

    fix map is null

--- src/com/main/july/week_5_exam/order_exam/dao/DatabaseHungry.java
+++ src/com/main/july/week_5_exam/order_exam/dao/DatabaseHungry.java
@@ -2,6 +2,7 @@ package com.main.july.week_5_exam.order_exam.dao;
 
 import com.main.july.week_5_exam.order_exam.entity.Order;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -16,7 +17,7 @@ public class DatabaseHungry {
     private static DatabaseHungry data = new DatabaseHungry();
 
     // 存储数据对象, key为订单ID value为订单对象
-    public Map<Long, Order> dbMap;
+    public Map<Long, Order> dbMap = new HashMap<>();
 
 
     /**
